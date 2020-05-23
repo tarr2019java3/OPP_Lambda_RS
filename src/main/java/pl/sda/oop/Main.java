73ab1d043e38ef51e2306ea8bf9c1e2dc2861976
Rafal Stasiorowski <rstasiorowski@gmail.com>
2020-05-23 14:10:18 +0200

    rev.6

--- src/main/java/pl/sda/oop/Main.java
+++ src/main/java/pl/sda/oop/Main.java
@@ -1,11 +1,18 @@
 package pl.sda.oop;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[]args){
-        List<String>names=new ArrayList<>();
+        Task task = new Task("Nauka programowania",
+                LocalDate.now(),
+                LocalDate.of(2020,9,30));
+        System.out.println(task.toString());
+
+        Task emptyTask = new Task();
+        emptyTask.setTaskName("empty task");
+        System.out.println(emptyTask.getTaskName());
+
 
     }
 }
--- src/main/java/pl/sda/oop/Task.java
+++ src/main/java/pl/sda/oop/Task.java
@@ -1,49 +1,20 @@
 package pl.sda.oop;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
 import java.time.LocalDate;
 
-public class Task {
+//Adnotacje lombok'a
+@AllArgsConstructor //implementuje konstruktor z wszystkimi argumentami
+@NoArgsConstructor  //implementuje pusty konstruktor
+@Data               //implementuje gettery, settery i toString()
+
+public class Task extends Object {
     private String taskName;
     private LocalDate startDate;
     private LocalDate stopDate;
-
-    public Task(String taskName, LocalDate startDate, LocalDate stopDate) {
-        this.taskName = taskName;
-        this.startDate = startDate;
-        this.stopDate = stopDate;
-    }
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "taskName='" + taskName + '\'' +
-                ", startDate=" + startDate +
-                ", stopDate=" + stopDate +
-                '}';
-    }
-    public void setTaskName(String taskName) {
-        this.taskName = taskName;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getStopDate() {
-        return stopDate;
-    }
-
-    public void setStopDate(LocalDate stopDate) {
-        this.stopDate = stopDate;
-    }
-
 
 }
