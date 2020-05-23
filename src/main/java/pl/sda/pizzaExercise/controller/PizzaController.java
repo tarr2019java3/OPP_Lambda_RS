dd79909af3e1392539845b7d3bfbf3c65b6b9cd7
Rafal Stasiorowski <rstasiorowski@gmail.com>
2020-05-23 11:13:16 +0200

    rev.1

---
+++ src/main/java/pl/sda/pizzaExercise/controller/PizzaController.java
@@ -0,0 +1,79 @@
+package pl.sda.pizzaExercise.controller;
+
+import pl.sda.pizzaExercise.model.Ingredients;
+import pl.sda.pizzaExercise.model.Pizza;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PizzaController {
+    /**
+     * Przekleić zawartość załączników do plików Ingredients.java i Pizza.java, uzupełnić brakujące importy.
+     * Każda Pizza posiada nazwę (name) i listę składników (ingredients). Każdy składnik posiada nazwę (name), cenę (price),
+     * informację czy jest to mięso (meat) (np pizza wegetariańska nie może posiadać żadnego składnika mięsnego),
+     * informację czy jest pikantny (spicy). Cena pizzy to suma cen wszystkich jej składników.
+     * • Zaimplementować klasę z następującymi metodami:
+     * o Pizza findCheapestSpicy() - metoda zwracająca najtańszą ostrą pizzę.
+     * o Pizza findMostExpensiveVegetarian() - metoda zwracająca najdroższą pizzę wegetariańską.
+     * o List iLikeMeat() - metoda zwracająca same pizzę mięsne, posortowane malejąco po liczbie składników mięsnych.
+     * o Map groupByPrice() - metoda grupujące pizzę po cenie.
+     * o String formatedMenu() - metoda zwracająca string w postaci nazwa_pizzy: składnik1, składnik2, składnik3
+     * - cena, kolejne pizzę oddzielone znakiem nowej linii.
+     * • Wszystkie metody zaimplementować z wykorzystaniem Stream API, najlepiej w postaci pojedynczego wyrażenia.
+     * Wewnątrz metod nie mogą się zanajdować instrukcje sterujące (warunki, pętle).
+     */
+
+
+// Wyszukaj i zwróć najtańszą pizze
+    public Pizza findCheapest() {
+        return Arrays.stream(Pizza.values())
+                .min(Comparator.comparing(this::getPizzaPrice)).get();
+
+    }
+
+    // Wyszukaj i zwróć najtańszą pizze ostra pizze
+    public Pizza findCheapestSpicy() {
+        return Arrays.stream(Pizza.values())
+                .filter(pizza -> pizza.getIngredients().stream().anyMatch(ingredients -> ingredients.isSpicy()))
+                .min(Comparator.comparing(this::getPizzaPrice)).get();
+
+    }
+
+    // Metoda zwracająca cenę pizzy podanej w argumencie metody
+    public double getPizzaPrice(Pizza pizza) {
+        return pizza.getIngredients()           // List<Ingerdeint>
+                .stream()       // Stream<Ingredient>
+                .mapToDouble(Ingredients::getPrice)   // Double
+                .sum();         // Double
+    }
+
+    //Metoda zwraca najdroższą pizze wegetariańską
+    public Pizza findMostExpensiveVegetarian() {
+        return Arrays.stream(Pizza.values()).filter(pizza -> pizza.getIngredients().stream()
+                .noneMatch(ingredients -> ingredients.isMeat())).max(Comparator.comparing(this::getPizzaPrice)).get();
+    }
+
+    //  - metoda zwracająca same pizzę mięsne, posortowane malejąco po liczbie składników mięsnych.
+    public List<Pizza> iLikeMeat() {
+        return Arrays.stream(Pizza.values())
+                .filter(pizza -> pizza.getIngredients()
+                        .stream().anyMatch(Ingredients::isMeat))
+                .sorted(Comparator.comparing(pizza -> pizza.getIngredients()
+                        .stream()
+                        .filter(ingredients -> ingredients.isMeat()).count(), Comparator.reverseOrder()))
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        PizzaController pc = new PizzaController();
+        System.out.println("najtańsza Pizza: " + pc.findCheapest() + " kosztuje");
+        System.out.println("Najtańsza pizza ostra to: " + pc.findCheapestSpicy());
+        System.out.println("Najdroższa wegetariańska pizza to : " + pc.findMostExpensiveVegetarian());
+        System.out.println("Lista mięsnych pizz posortowancyh po ilości skłaników mięsnych: ");
+        pc.iLikeMeat().forEach(pizza -> System.out.println(pizza
+                + " " + pizza.getIngredients().size()
+                + " " + pizza.getIngredients().stream().filter(Ingredients::isMeat).count()));
+    }
+}
---
+++ src/main/java/pl/sda/pizzaExercise/model/Pizza.java
@@ -0,0 +1,48 @@
+package pl.sda.pizzaExercise.model;
+
+import java.util.Arrays;
+import java.util.List;
+import static pl.sda.pizzaExercise.model.Ingredients.*;
+
+
+public enum Pizza {
+    MARGHERITA("Marrgherita", THIN_CRUST, TOMATO_SUACE, CHEESE),
+    CAPRI("Capri", THIN_CRUST, TOMATO_SUACE, CHEESE, HAM, MUSHROOMS),
+    HAVAI("Havai", THIN_CRUST, TOMATO_SUACE, CHEESE, HAM, PINEAPPLE),
+    CARUSO("Caruso", THIN_CRUST, TOMATO_SUACE, SUASAGE, PEPERONI),
+    MAMA_MIA("Mama Mia", THIN_CRUST, TOMATO_SUACE, CHEESE, ONION, MUSHROOMS, BECON),
+    SOPRANO("Soprano", THICK_CRUST, TOMATO_SUACE, CHEESE, HAM, MUSHROOMS, ONION, BECON, PEPPER),
+    CALABRESE("Calabrese", THICK_CRUST, TOMATO_SUACE, CHEESE,
+            HAM, MUSHROOMS, SUASAGE, ONION, OLIVES),
+    VEGETARIANA("Vegetariana", THIN_CRUST, TOMATO_SUACE, CHEESE,
+            ONION, BEAN, CORN, BROCCOLI, ARUGULA),
+    CAPRESE("Caprese", THICK_CRUST, TOMATO_SUACE, MOZARELLA, FETA, TOMATO, BASIL),
+    PASCETORE("Pascetore", THIN_CRUST, TOMATO_SUACE, CHEESE, TUNA, ONION),
+    FOUR_CHEESE("Cztery sery", THIN_CRUST, TOMATO_SUACE, CHEESE, MOZARELLA, FETA, BLUE_CHEESE),
+    TABASCO("Tabasco", THICK_CRUST, TOMATO_SUACE, CHEESE, HAM, SALAMI, PEPERONI, CORN, TABASCO_SUACE),
+    AMORE("Amore", THIN_CRUST, TOMATO_SUACE, CHEESE, CHICKEN, TOMATO),
+    FARMER("Farmerska", THICK_CRUST, TOMATO_SUACE, CHEESE,
+            CHICKEN, BECON, ONION, CORN);
+    private final String name;
+    private final List<Ingredients> ingredients;
+
+    private Pizza(String name, Ingredients... ingredients) {
+        this.name = name;
+        this.ingredients = Arrays.asList(ingredients);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Ingredients> getIngredients() {
+        return ingredients;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+
+
